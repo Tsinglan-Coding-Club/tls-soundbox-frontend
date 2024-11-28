fb0b2f0a0f25a2031a769bff603ad00f63d16c4d
Qizheng  Wang <qizhengwangcharles@gmail.com>
2024-11-28 19:24:48 +0800

    change login request

--- login.js
+++ login.js
@@ -5,26 +5,6 @@ function setCookies(token) {
 }
 
 document.getElementById('login-btn').addEventListener('click', function() {
-    let res = login();
-    if(res){
-        setCookies(res);
-        window.location.replace("/soundboxBooking.html")
-    }else{
-        window.alert("network error");
-    }
-});
-
-async function login(){
     let url = "https://soundbox.v1an.xyz/login";
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-        });
-        const result = await response.json();
-        return result.toString();
-    } catch (e) {
-        console.error("Error on getting:", e);
-        return null;
-    }
-}
+    window.location.replace(url);
+});
